64c9704fba6d6d5b4bd807cb700f97c841188606
raeunlee <raeun19@naver.com>
2024-08-03 14:44:50 +0900

    feat: ExchangeRate Entity

--- src/main/java/com/fininco/finincoserver/exchange/entity/ExchangeRate.java
+++ src/main/java/com/fininco/finincoserver/exchange/entity/ExchangeRate.java
@@ -1,14 +1,71 @@
+/*
+ * 
+ * 환율 Entity
+ * 
+ * 환율 식별번호, 환율 기준시간, 환율 국가, 환율 가격
+ * 
+ */
+
+
+
+
 package com.fininco.finincoserver.exchange.entity;
 
 import java.time.LocalDateTime;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+
+
+@Entity
+@Getter
+@NoArgsConstructor
+@Table(name = "ExchangeRate")
 public class ExchangeRate {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+	
+	@Column(name = "dateTime")
+	private LocalDateTime dateTime;
+	
+	@Column(name = "country")
+    private String country;
+	
 
-    Long id;
+	@Column(name = "price")
+	private long price;
+	
 
-    LocalDateTime dateTime;
+	public Long getId() {
+		return id;
+	}
 
-    Country country;
+
+	public LocalDateTime getDateTime() {
+		return dateTime;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+
+	public long getPrice() {
+		return price;
+	}
+
+
+	public ExchangeRate(Long id, LocalDateTime dateTime, String country, long price) {
+		super();
+		this.id = id;
+		this.dateTime = dateTime;
+		this.country = country;
+		this.price = price;
+	}
 
 
 }
