f077ed19aeea218fbb8043d250659aaae07aaee0
na-yk <ninghao17@naver.com>
2024-08-03 10:47:44 +0900

    init: 프로젝트 세팅

---
+++ src/main/java/com/fininco/finincoserver/FinincoServerApplication.java
@@ -0,0 +1,13 @@
+package com.fininco.finincoserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinincoServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinincoServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fininco/finincoserver/FinincoServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fininco.finincoserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinincoServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
