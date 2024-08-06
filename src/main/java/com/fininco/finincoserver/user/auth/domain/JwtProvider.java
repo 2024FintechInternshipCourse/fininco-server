0c430a8be1ee62f909b2f8587113ea05fdec4532
na-yk <ninghao17@naver.com>
2024-08-06 19:44:19 +0900

    feat: #3 JWT 토큰 발급 로직 구현

---
+++ src/main/java/com/fininco/finincoserver/user/auth/domain/JwtProvider.java
@@ -0,0 +1,35 @@
+package com.fininco.finincoserver.user.auth.domain;
+
+import com.fininco.finincoserver.user.auth.config.JwtProperties;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Component;
+import com.fininco.finincoserver.user.entity.User;
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+
+@Component
+public class JwtProvider {
+
+    private final JwtProperties jwtProperties;
+    private final SecretKey key;
+
+    public JwtProvider(JwtProperties jwtProperties) {
+        this.jwtProperties = jwtProperties;
+        this.key = Keys.hmacShaKeyFor(jwtProperties.getSecretKey().getBytes(StandardCharsets.UTF_8));
+    }
+
+    public String provideAccessToken(User user) {
+        Date now = new Date();
+        Date expiredAt = new Date(now.getTime() + jwtProperties.getAccessTokenExpiration());
+
+        return Jwts.builder()
+                .subject(user.getId())
+                .issuedAt(now)
+                .expiration(expiredAt)
+                .signWith(key)
+                .compact();
+    }
+
+}
