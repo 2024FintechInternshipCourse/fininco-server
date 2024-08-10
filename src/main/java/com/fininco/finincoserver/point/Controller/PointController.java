7290b9366a4f475e17cc36ab5449d623516a1091
raeunlee <raeun19@naver.com>
2024-08-10 22:20:04 +0900

    feat: 포인트 내역 조회 api

--- src/main/java/com/fininco/finincoserver/point/Controller/PointController.java
+++ src/main/java/com/fininco/finincoserver/point/Controller/PointController.java
@@ -6,10 +6,9 @@ import com.fininco.finincoserver.point.dto.response.PointHistoryResponse;
 import com.fininco.finincoserver.point.service.PointService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/points")
@@ -24,6 +23,22 @@ public class PointController {
     @PostMapping("/charge")
     public ResponseEntity<PointHistoryResponse> chargePoint(@RequestBody PointHistoryCreateRequest request) {
         PointHistoryResponse response = pointService.chargePoint(request);
+        return ResponseEntity.ok(response);
+    }
+
+    /**
+     * 포인트 내역 조회
+     * @param userId 조회할 사용자 ID
+     * @param period 조회할 기간 (기본값: 오늘날짜로 한달)
+     * @return 포인트 내역 목록
+     */
+
+    @GetMapping("/history")
+    public ResponseEntity<List<PointHistoryResponse>> getPointHistories(
+            @RequestParam String userId,
+            @RequestParam(required = false, defaultValue = "1month") String period
+    ) {
+        List<PointHistoryResponse> response = pointService.getPointHistories(userId, period);
         return ResponseEntity.ok(response);
     }
 }
